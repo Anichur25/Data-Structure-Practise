fd9fe88f2d9385bcdc2e4cf77b7d6506cec82a9e
Anichur25 <aniscseru6125@gmail.com>
2018-09-02 12:17:48 +0600

    One new problem solved

---
+++ Uva Online Judge/Uva12100.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool compare(pair<int,int> a, pair<int,int> b){
+
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first)
+        return true;
+    else false;
+}
+
+
+int main(){
+
+  int testCases;
+  int priority;
+  int jobNo;
+  int n,m;
+
+  scanf("%d",&testCases);
+
+  while(testCases--){
+
+     queue<pair<int,int> > Order;
+     priority_queue<int> Q;
+
+     scanf("%d%d",&n,&m);
+
+     for(int i = 0; i < n; i++){
+        scanf("%d",&priority);
+        Order.push(make_pair(priority,i));
+        Q.push(priority);
+     }
+
+     int ans = 0;
+     bool found = false;
+
+     while(!Q.empty()){
+        int top = Q.top();
+        Q.pop();
+
+        while(!Order.empty()){
+
+            pair<int,int> Front = Order.front();
+
+            if(Front.first == top){
+                ++ans;
+                Order.pop();
+                if(Front.second == m){
+                    found = true;
+                }
+                break;
+            }
+            else
+            {
+                Order.pop();
+                Order.push(Front);
+            }
+        }
+
+        if(found) break;
+     }
+
+     printf("%d\n",ans);
+  }
+  return 0;
+}
+
+
